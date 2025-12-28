05eace5be08b221e05798766df5fd11856cab18b
rohit-ranjith <rohitranjithkumar5@gmail.com>
2025-12-28 00:44:29 +0000

    updating directory structure

--- collector/pull_cowrie_logs.sh
+++ collector/pull_cowrie_logs.sh
@@ -7,6 +7,6 @@ LOCAL_DIR="$HOME/phantomnet/data/raw/cowrie_snapshots/"
 
 mkdir -p "$LOCAL_DIR"
 
-rsync -avz --ignore-existing \
+rsync -av -e "ssh -p 22222" \
   ${AZURE_USER}@${AZURE_IP}:${AZURE_DIR} \
   "$LOCAL_DIR"
