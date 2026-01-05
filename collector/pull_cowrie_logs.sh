305fe9c2ea180d3294dfbef44e30bd8ce0566e90
rohit-ranjith <126744602+rohit-ranjith@users.noreply.github.com>
2026-01-04 18:05:54 -0600

    Update pull_cowrie_logs.sh

--- collector/pull_cowrie_logs.sh
+++ collector/pull_cowrie_logs.sh
@@ -8,6 +8,6 @@ LOCAL_DIR="$HOME/phantomnet/data/raw/cowrie_snapshots/"
 
 mkdir -p "$LOCAL_DIR"
 
-rsync -av -e "ssh -p 22222" \
+rsync -av -e "ssh -p (ssh port on vm)" \
   ${AZURE_USER}@${AZURE_IP}:${AZURE_DIR} \
   "$LOCAL_DIR"
